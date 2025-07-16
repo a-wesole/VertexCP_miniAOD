2e7891d8fd774fe36298e01888f3f7b6cbca2508
awesole <awesole@purdue.edu>
2025-07-16 15:46:22 -0400

    up

--- VertexCompositeProducer/test/crab/D0_data_submit.py
+++ VertexCompositeProducer/test/crab/D0_data_submit.py
@@ -1,26 +1,23 @@
 from CRABClient.UserUtilities import config #, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'test_1_file'
-config.General.workArea = 'test_1_file'
+config.General.workArea = 'D0_2023PbPb'
+config.General.requestName = 'HIPhysicsRawPrime0_D0_16July2025'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '/home/awesole/VertexCP_2/CMSSW_13_2_11/src/VertexCompositeAnalysis/VertexCompositeProducer/test/run_edm_and_ttree_DATA.py'
 config.JobType.allowUndistributedCMSSW = True
-#config.JobType.maxMemoryMB = 4500
 
 config.Data.inputDataset = '/HIPhysicsRawPrime0/HIRun2023A-PromptReco-v2/MINIAOD' 
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
-config.Data.unitsPerJob = 1 
-config.Data.lumiMask = 'mini.json'
-#for part of dataset0
-config.Data.outLFNDirBase = '/store/user/awesolek/test_1_file' 
-#config.Data.publication = True
-config.Data.publication = False
-config.Data.outputDatasetTag = 'test_1_file'
+config.Data.unitsPerJob = 4 
+config.Data.lumiMask = 'Cert_Collisions2023HI_374288_375823_Golden.json'
+config.Data.outLFNDirBase = '/store/user/awesolek/D0_2023PbPb' 
+config.Data.publication = True
+config.Data.outputDatasetTag = 'HIPhysicsRawPrime0_D0_16July2025'
 
 config.Site.storageSite = 'T2_US_Purdue'
 #config.Site.ignoreGlobalBlacklist = True
